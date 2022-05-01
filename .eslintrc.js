2d8f1fd44db3b5efe8511672db834fe19d359d90
RigelTLT <kozyrin-di@yandex.ru>
2022-05-02 02:20:24 +0400

    init: start virtual-keyboard-task

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "airbnb-typescript/base",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": "./tsconfig.json",
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+      ],
+    "rules": {
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HTML_WEBPACK_PLUGIN = require('html-webpack-plugin');
+const COPY_PLUGIN = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const eslintPlugin = require('eslint-webpack-plugin'); 
+
+const devServer = (isDev) => !isDev ? {} : {
+    devServer: {
+      open: true,
+      port: 8080,
+      contentBase: path.join(__dirname, 'public'),
+    },
+  };
+  
+  const esLintPlugin = (isDev) => isDev ? [] : [ new eslintPlugin({ extensions: ['ts', 'js'] }) ];
+
+  module.exports = ({ development }) => ({
+    mode: development ? 'development' : 'production',
+    devtool: development ? 'inline-source-map' : false,
+    entry: {
+      main: './src/index.ts',
+    },
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      assetModuleFilename: 'assets/[hash][ext]',
+    },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]s?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|png|svg|jpg|jpeg)$/i,
+        type: 'assets/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"] 
+    },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  plugins: [
+    ...esLintPlugin(development),
+    new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css'
+    }),
+    new HTML_WEBPACK_PLUGIN({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAsset: false }),
+  ],
+  ...devServer(development)
+});
