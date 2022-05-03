c558c96c05fcf84f35146be7958b4972b3f476bd
RigelTLT <kozyrin-di@yandex.ru>
2022-05-03 15:10:05 +0400

    feat: add function "Caps" "Change language"

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import './style.scss';
-const body = document.body;
-const wrapper = document.createElement('div');
-wrapper.classList.add('wrapper');
-body.append(wrapper);
-const texareaDiv = document.createElement('div');
-texareaDiv.classList.add('text-block');
-wrapper.append(texareaDiv);
-const textarea = document.createElement('textarea');
-textarea.classList.add('text-block__textarea');
-texareaDiv.append(textarea);
-const keyboard = document.createElement('div');
-keyboard.classList.add('keyboard');
-wrapper.append(keyboard);
+let lang = 'rus';
+function setLocalStorage() {
+  localStorage.setItem('lang', lang);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+function getLocalStorage() {
+  if (localStorage.getItem("lang")) {
+    let langSt = localStorage.getItem('lang');
+    lang = changeLanguage(langSt)
+  }
+}
+window.addEventListener('load', getLocalStorage);
 //англ мал
 
 //[33, 49, 64, 50, 35, 51, 36, 52, 37, 53, 94, 54, 38, 55, 42, 56, 40, 57, 41, 48, 95, 45, 43, 61]
@@ -88,74 +87,168 @@ wrapper.append(keyboard);
   'CtrlR',
 ];*/
 const rusSmall = [
-  [192 , 1105],
-  [49 , [33, 49]],
-  [50 , [34, 50]],
-  [51 , [8470, 51]],
-  [52 , [59, 52]],
-  [53 , [37, 53]],
-  [54 , [58, 54]],
-  [55 , [63, 55]],
-  [56 , [42, 56]],
-  [57 , [40, 57]],
-  [48 , [41, 48]],
-  [189 , [95, 45]],
-  [187 , [43, 61]],
+  [192, 1105],
+  [49, [33, 49]],
+  [50, [34, 50]],
+  [51, [8470, 51]],
+  [52, [59, 52]],
+  [53, [37, 53]],
+  [54, [58, 54]],
+  [55, [63, 55]],
+  [56, [42, 56]],
+  [57, [40, 57]],
+  [48, [41, 48]],
+  [189, [95, 45]],
+  [187, [43, 61]],
   ['Backspace', 'Backspace'],
   ['Tab', 'Tab'],
-  [81 , 1081],
-  [87 , 1094],
-  [69 , 1091],
-  [82 , 1082],
-  [84 , 1077],
-  [89 , 1085],
-  [85 , 1075],
-  [73 , 1096],
-  [79 , 1097],
-  [80 , 1079],
- [219 , 1093],
-  [221 , 1098],
-  ['Delete' , 'Del'],
-  ['CapsLock' , 'Caps'],
-  [65 , 1092],
-  [83 , 1099],
-  [68 , 1074],
-  [70 , 1072],
-  [71 , 1087],
-  [72 , 1088],
-  [74 , 1086],
-  [75 , 1083],
-  [76 , 1076],
-  [186 , 1078],
-  [222 , 1101],
-  [220 , 92],
-  ['Enter' , 'Enter'],
- ['ShiftLeft' ,  'ShiftL'],
-  [90 , 1103],
-  [88 , 1095],
-  [67 , 1089],
-  [86 , 1084],
-  [66 , 1080],
-  [78 , 1090],
-  [77 , 1100],
-  [188 , 1073],
-  [190 , 1102],
-  [191 , 46],
-  ['ShiftRight' , 'ShiftR'],
-  ['ControlLeft' , 'CtrlL'],
-  ['MetaLeft' , 'Win'],
-  ['AltLeft' , 'AltL'],
-  [32 , ''],
-  ['AltRight' , 'AltR'],
-  ['ControlRight' , 'CtrlR']
+  [81, 1081],
+  [87, 1094],
+  [69, 1091],
+  [82, 1082],
+  [84, 1077],
+  [89, 1085],
+  [85, 1075],
+  [73, 1096],
+  [79, 1097],
+  [80, 1079],
+  [219, 1093],
+  [221, 1098],
+  ['Delete', 'Del'],
+  ['CapsLock', 'Caps'],
+  [65, 1092],
+  [83, 1099],
+  [68, 1074],
+  [70, 1072],
+  [71, 1087],
+  [72, 1088],
+  [74, 1086],
+  [75, 1083],
+  [76, 1076],
+  [186, 1078],
+  [222, 1101],
+  [220, 92],
+  ['Enter', 'Enter'],
+  ['ShiftLeft', 'ShiftL'],
+  [90, 1103],
+  [88, 1095],
+  [67, 1089],
+  [86, 1084],
+  [66, 1080],
+  [78, 1090],
+  [77, 1100],
+  [188, 1073],
+  [190, 1102],
+  [191, 46],
+  ['ShiftRight', 'ShiftR'],
+  ['ControlLeft', 'CtrlL'],
+  ['MetaLeft', 'Win'],
+  ['AltLeft', 'AltL'],
+  [32, ''],
+  ['AltRight', 'AltR'],
+  ['ControlRight', 'CtrlR'],
+];
+//[33, 49, 64, 50, 35, 51, 36, 52, 37, 53, 94, 54, 38, 55, 42, 56, 40, 57, 41, 48, 95, 45, 43, 61]
+//[[96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 92, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47]
+
+const enSmall = [
+  [192, 96],
+  [49, [33, 49]],
+  [50, [64, 50]],
+  [51, [35, 51]],
+  [52, [36, 52]],
+  [53, [37, 53]],
+  [54, [94, 54]],
+  [55, [38, 55]],
+  [56, [42, 56]],
+  [57, [40, 57]],
+  [48, [41, 48]],
+  [189, [95, 45]],
+  [187, [43, 61]],
+  ['Backspace', 'Backspace'],
+  ['Tab', 'Tab'],
+  [81, 113],
+  [87, 119],
+  [69, 101],
+  [82, 114],
+  [84, 116],
+  [89, 121],
+  [85, 117],
+  [73, 105],
+  [79, 111],
+  [80, 112],
+  [219, 91],
+  [221, 93],
+  ['Delete', 'Del'],
+  ['CapsLock', 'Caps'],
+  [65, 97],
+  [83, 115],
+  [68, 100],
+  [70, 102],
+  [71, 103],
+  [72, 104],
+  [74, 106],
+  [75, 107],
+  [76, 108],
+  [186, 59],
+  [222, 39],
+  [220, 92],
+  ['Enter', 'Enter'],
+  ['ShiftLeft', 'ShiftL'],
+  [90, 122],
+  [88, 120],
+  [67, 99],
+  [86, 118],
+  [66, 98],
+  [78, 110],
+  [77, 109],
+  [188, 44],
+  [190, 46],
+  [191, 47],
+  ['ShiftRight', 'ShiftR'],
+  ['ControlLeft', 'CtrlL'],
+  ['MetaLeft', 'Win'],
+  ['AltLeft', 'AltL'],
+  [32, ''],
+  ['AltRight', 'AltR'],
+  ['ControlRight', 'CtrlR'],
 ];
 
-const arrArrow = ['',['ArrowUp', 8593], '', ['ArrowLeft', 8592], ['ArrowDown', 8595], ['ArrowRight', 8594]];
+const body = document.body;
+const wrapper = document.createElement('div');
+wrapper.classList.add('wrapper');
+body.append(wrapper);
+const texareaDiv = document.createElement('div');
+texareaDiv.classList.add('text-block');
+wrapper.append(texareaDiv);
+const textarea = document.createElement('textarea');
+textarea.classList.add('text-block__textarea');
+texareaDiv.append(textarea);
+const keyboard = document.createElement('div');
+keyboard.classList.add('keyboard');
+wrapper.append(keyboard);
+
+let arrLang = [];
+if (lang === 'rus') {
+  arrLang = rusSmall;
+}
+if (lang === 'en') {
+  arrLang = enSmall;
+}
+const arrArrow = [
+  '',
+  ['ArrowUp', 8593],
+  '',
+  ['ArrowLeft', 8592],
+  ['ArrowDown', 8595],
+  ['ArrowRight', 8594],
+];
 const rowButton = 5;
 const colButton = 14;
 const colButton2 = 12;
 const colButton3 = 7;
 let indexKey = 0;
+let corret =textarea.selectionStart;
 for (let i = 0; i < rowButton; i++) {
   let maxCol = colButton;
   if (i === 3) {
@@ -179,7 +272,7 @@ for (let i = 0; i < rowButton; i++) {
         if (typeof arrArrow[indexArrow] === 'object') {
           const key = document.createElement('span');
           key.classList.add('button-key__span');
-          inputArr.dataset.id =arrArrow[indexArrow][0];
+          inputArr.dataset.id = arrArrow[indexArrow][0];
           key.innerHTML = String.fromCharCode(Number(arrArrow[indexArrow][1]));
           inputArr.append(key);
         }
@@ -188,65 +281,147 @@ for (let i = 0; i < rowButton; i++) {
     } else {
       const key = document.createElement('span');
       key.classList.add('button-key__span');
-      
-      if (typeof rusSmall[indexKey][1] === 'number') {
-        button.dataset.id = rusSmall[indexKey][0];
-        key.innerHTML = String.fromCharCode(Number(rusSmall[indexKey][1]));
+
+      if (typeof arrLang[indexKey][1] === 'number') {
+        key.classList.add('button-key__span__letter');
+        button.dataset.id = arrLang[indexKey][0];
+        key.innerHTML = String.fromCharCode(Number(arrLang[indexKey][1]));
       }
-      if (typeof rusSmall[indexKey][1] === 'string') {
-        button.dataset.id = rusSmall[indexKey][0];
-        key.innerHTML = String(rusSmall[indexKey][1]);
+      if (typeof arrLang[indexKey][1] === 'string') {
+        button.dataset.id = arrLang[indexKey][0];
+        key.innerHTML = String(arrLang[indexKey][1]);
       }
-      if (typeof rusSmall[indexKey][1] === 'object') {
+      if (typeof arrLang[indexKey][1] === 'object') {
         const keyTwo = document.createElement('span');
         keyTwo.classList.add('button-key__spanUp');
-        keyTwo.innerHTML = String.fromCharCode(Number(rusSmall[indexKey][1][0]));
+        keyTwo.innerHTML = String.fromCharCode(Number(arrLang[indexKey][1][0]));
         button.append(keyTwo);
-        button.dataset.id = rusSmall[indexKey][0];
-        key.innerHTML = String.fromCharCode(Number(rusSmall[indexKey][1][1]));
+        button.dataset.id = arrLang[indexKey][0];
+        key.innerHTML = String.fromCharCode(Number(arrLang[indexKey][1][1]));
       }
       button.append(key);
       indexKey++;
     }
   }
 }
+function changeLanguage(lang) {
+  const allKeys = document.querySelectorAll('.button-key__span');
+  const allKeys2 = document.querySelectorAll('.button-key__spanUp');
+  let arrLang = [];
+  if (lang === 'rus') {
+    arrLang = enSmall;
+  }
+  if (lang === 'en'){
+    arrLang = rusSmall;
+  }
+  localStorage.setItem('lang', lang);
+  for(let index = 0; index < allKeys.length; index++){
+    if (typeof arrLang[index][1] === 'number') {
+      allKeys[index].innerHTML = String.fromCharCode(Number(arrLang[index][1]));
+    }
+    /*if (typeof arrLang[index][1] === 'string') {
+      allKeys[index].innerHTML = String(arrLang[index][1]);
+    }*/
+    if (typeof arrLang[index][1] === 'object') {
+      allKeys2[index-1].innerHTML = String.fromCharCode(
+        Number(arrLang[index][1][0])
+      );
+      allKeys[index].innerHTML = String.fromCharCode(Number(arrLang[index][1][1]));
+    }
+    if(allKeys[index].innerHTML === 'CtrlR'){
+      break;
+    }
+  }
+  if (lang === 'rus') {
+    return lang = 'en';
+  }
+  if (lang === 'en'){
+    return lang = 'rus';
+  }
+}
 function keyDownListener(event) {
-  console.log(event);
-  let specialKey = {
-    'Backspace': 'Backspace',
-    'Tab': 'Tab',
-    'Delete': 'Del',
-    'CapsLock':'Caps',
-    'ShiftLeft':'ShiftL',
-    'ShiftRight':'ShiftR',
-    'ControlLeft':'CtrlL',
-    'MetaLeft':'Win',
-    'AltLeft':'AltL',
-    'Space':'',
-    'AltRight':'AltR',
-    'ControlRight':'CtrlR',
-    'Enter':'Enter',
-    'ArrowUp':'8593',
-    'ArrowLeft':'8592',
-    'ArrowDown':'8595',
-    'ArrowRight':'8594',
-  }
   
-  if(specialKey[`${event.code}`]){
-    document.querySelector(`.button-key[data-id="${event.code}"]`).classList.add('active_button');
-  }
-  else{
-    document.querySelector(`.button-key[data-id="${event.keyCode}"]`).classList.add('active_button');
-    const letter = document.querySelector(`.button-key[data-id="${event.keyCode}"] .button-key__span`)
-    console.log();
+  let specialKey = {
+    Backspace: 'Backspace',
+    Tab: 'Tab',
+    Delete: 'Del',
+    CapsLock: 'Caps',
+    ShiftLeft: 'ShiftL',
+    ShiftRight: 'ShiftR',
+    ControlLeft: 'CtrlL',
+    MetaLeft: 'Win',
+    AltLeft: 'AltL',
+    Space: '',
+    AltRight: 'AltR',
+    ControlRight: 'CtrlR',
+    Enter: 'Enter',
+    ArrowUp: '8593',
+    ArrowLeft: '8592',
+    ArrowDown: '8595',
+    ArrowRight: '8594',
+  };
+
+  if (specialKey[`${event.code}`]) {
+    document
+      .querySelector(`.button-key[data-id="${event.code}"]`)
+      .classList.add('active_button');
+    if (event.code === 'AltLeft') {
+      document.onkeyup = function (el) {
+        if (el.code === 'ShiftLeft') {
+          lang = changeLanguage(lang);
+        } else {
+          window.removeEventListener('keydown', keyDownListener);
+        }
+      };
+    }
+    if (event.code === 'CapsLock') {
+      const letters = document.querySelectorAll('.button-key__span__letter');
+      letters.forEach(function (element) {
+        element.classList.toggle("capslock");
+      })
+      window.removeEventListener('keydown', keyDownListener);
+    }
+    if (event.code === 'Backspace') {
+     if (textarea.selectionStart === 0 && textarea.innerHTML){
+      textarea.innerHTML =  textarea.innerHTML.slice(0, -1);
+      }
+      else{
+        corret =textarea.selectionStart;
+        textarea.innerHTML =  textarea.innerHTML.slice(0, textarea.selectionStart-1)+  textarea.innerHTML.slice(textarea.selectionStart)
+        textarea.selectionStart = corret-1;
+      }
+    }
+    if (event.code === 'Delete') {
+      corret =textarea.selectionStart;
+        textarea.innerHTML =  textarea.innerHTML.slice(0, textarea.selectionStart)+  textarea.innerHTML.slice(textarea.selectionStart+1)
+        textarea.selectionStart = corret;
+
+    } 
+  } else {
+    document
+      .querySelector(`.button-key[data-id="${event.keyCode}"]`)
+      .classList.add('active_button');
+      
+    const letter = document.querySelector(
+      `.button-key[data-id="${event.keyCode}"] .button-key__span`
+    );
+    console.log(letter.innerHTML);
+    console.log(textarea.innerHTML);
     textarea.innerHTML += letter.innerHTML;
   }
 }
+
 window.addEventListener('keydown', keyDownListener);
-window.addEventListener('keyup', function() {
-  document.querySelectorAll('.button-key').forEach(function(el){
+window.addEventListener('keyup', function () {
+  document.querySelectorAll('.button-key').forEach(function (el) {
     el.classList.remove('active_button');
-  })
+  });
+  window.addEventListener('keydown', keyDownListener);
 });
+
+textarea.addEventListener('blur', function (event) {
+  event.preventDefault();
+  window.addEventListener('keydown', keyDownListener);
+}, false)
 
 //document.querySelector('.button-key__span').style.textTransform  = 'uppercase';
